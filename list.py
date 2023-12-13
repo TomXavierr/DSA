4106c6f2766acd9bda2723c178bd7b22692f58a3
Tom Xavier <tomxavieronthego@gmail.com>
2023-12-13 14:56:44 +0530

    first commit

---
+++ list.py
@@ -0,0 +1,21 @@
+
+list = [1,2,3,4,5]
+
+print(list)
+
+list[1] = 6
+print(list)
+
+list.insert(1,8)
+print(list)
+
+list.sort()
+print(list)
+
+list.append(10)
+print(list)
+
+list.reverse()
+print(list)
+
+print(list[-5:-1])
