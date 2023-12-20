f0826e413ec771b75588dcfc4e6bfa60fd86ac6f
Tom Xavier <tomxavieronthego@gmail.com>
2023-12-20 23:20:04 +0530

    Leetcode 2706 different solutions

--- Leetcode_2706.py
+++ Leetcode_2706.py
@@ -7,7 +7,31 @@ class Solution:
         else:
             return money
 
+    def buyCandy(self, prices, money):
+        lowest = min(prices)
+        prices.remove(lowest)
+        second_lowest = min(prices)
+
+        balance = money - (lowest + second_lowest)
+
+        return balance if balance >= 0 else money
+
+    def buyChoco2(self, prices, money):
+        lowest = second_lowest = float("inf")
+
+        for price in prices:
+            if price < lowest:
+                lowest, second_lowest = price, lowest
+            else:
+                second_lowest = min(second_lowest, price)
+
+        balance = money - (lowest + second_lowest)
+        print("balance", balance)
+        
+        return balance if balance >= 0 else money
+
+
 
 sol = Solution()
 
-print(sol.buyChoco([3,2,3], 3))
+print(sol.buyChoco2([3,2,5], 10))
