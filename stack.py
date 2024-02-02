f5891b737dc9201239022e55a16250d7777a972a
Tom Xavier <tomxavieronthego@gmail.com>
2024-02-02 22:41:41 +0530

    initialized stack data structure

---
+++ stack.py
@@ -0,0 +1,9 @@
+class stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
