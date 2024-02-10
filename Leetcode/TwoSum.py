21a3d6b3f71b3fc2219a2f0e5f98b3fba196715a
Tom Xavier <tomxavieronthego@gmail.com>
2024-02-10 17:01:55 +0530

    two sum, hashmap method

--- Leetcode/TwoSum.py
+++ Leetcode/TwoSum.py
@@ -1,8 +1,24 @@
-def twoSum(list, target):
-    for i in range(len(list)):
-        for j in range(len(list)):
-            if list[i]+list[j] == target and i != j:
-                return [i,j]
+def twoSum(nums, target):
+    # for i in range(len(list)):
+    #     for j in range(i+1, len(list)):
+    #         if list[i]+list[j] == target and i != j:
+    #             return [i,j]
+
+
+    prev_nums = {}
+    for i,n in enumerate(nums):
+        if target-n in prev_nums:
+            return [i,prev_nums[target-n]]
+        else:
+            prev_nums[n] = i
+
+
+
+
+
+
+
+
 
 
 print(twoSum([2,7,11,15],9))
